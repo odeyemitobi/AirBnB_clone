25712fbde7b1519ec30be80c576c27754eb7cc4f
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-03-08 16:25:24 +0100

    Added kwargs arguement

--- models/base_model.py
+++ models/base_model.py
@@ -3,9 +3,24 @@ from datetime import datetime
 
 
 class BaseModel:
+
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        setattr(
+                            self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                        )
+                    else:
+                        setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
